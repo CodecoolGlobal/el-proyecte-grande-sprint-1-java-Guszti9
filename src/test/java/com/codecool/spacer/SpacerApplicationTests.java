ba68f9e37f95c99e10ca0978a04dc6a8101c2e33
benedekmolnar <molnar.benedek@gmail.com>
2022-05-17 10:53:28 +0200

    Initialize project

---
+++ src/test/java/com/codecool/spacer/SpacerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.spacer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
