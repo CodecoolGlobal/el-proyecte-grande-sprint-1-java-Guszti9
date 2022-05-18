91ee2a5d845b4b4bab5adca8f3b6774b5a033c4b
galambati <galambosati98@gmail.com>
2022-05-18 10:42:56 +0200

    Initialize UserDao interface with methods

--- src/main/java/com/codecool/spacer/dao/UserDao.java
+++ src/main/java/com/codecool/spacer/dao/UserDao.java
@@ -1,4 +1,9 @@
 package com.codecool.spacer.dao;
 
+import com.codecool.spacer.model.User;
+
 public interface UserDao {
+    void addUser(User user);
+    void editUser(User user, int userId);
+    void deleteUser(int userId);
 }
--- src/main/java/com/codecool/spacer/dao/mem/UserDaoMem.java
+++ src/main/java/com/codecool/spacer/dao/mem/UserDaoMem.java
@@ -1,4 +1,25 @@
 package com.codecool.spacer.dao.mem;
 
-public class UserDaoMem {
+import com.codecool.spacer.dao.UserDao;
+import com.codecool.spacer.model.User;
+
+import java.util.List;
+
+public class UserDaoMem implements UserDao {
+
+    List<User> allUsers;
+    @Override
+    public void addUser(User user){
+
+    }
+
+    @Override
+    public void editUser(User user, int userId) {
+
+    }
+    @Override
+    public void deleteUser(int userId){
+
+    }
+
 }
