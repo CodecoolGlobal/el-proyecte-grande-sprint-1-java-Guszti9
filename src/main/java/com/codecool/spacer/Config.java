0ad40ac7e40f5153c50b2601861a9d81e0cdba40
benedekmolnar <molnar.benedek@gmail.com>
2022-05-18 09:59:05 +0200

    Add config class for later use

---
+++ src/main/java/com/codecool/spacer/Config.java
@@ -0,0 +1,8 @@
+package com.codecool.spacer;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Config {
+
+}
--- src/main/java/com/codecool/spacer/model/User.java
+++ src/main/java/com/codecool/spacer/model/User.java
@@ -8,7 +8,7 @@ public class User {
     private static int idCounter = 0;
     private int id = idCounter++;
 
-    private String firsName;
+    private String firstName;
     private String lastName;
     private Path picture;
     private String description;
@@ -16,11 +16,11 @@ public class User {
     private String phoneNumber;
     private String email;
     private String linkedIn;
-    private String facebook;
+    private String faceBook;
     private List<SpaceShip> ships;
 
     public User(String firsName, String lastName, Path picture, String description, String password, String phoneNumber, String email, String linkedIn, String facebook) {
-        this.firsName = firsName;
+        this.firstName = firsName;
         this.lastName = lastName;
         this.picture = picture;
         this.description = description;
@@ -28,7 +28,7 @@ public class User {
         this.phoneNumber = phoneNumber;
         this.email = email;
         this.linkedIn = linkedIn;
-        this.facebook = facebook;
+        this.faceBook = facebook;
         ships = new ArrayList<>();
     }
 }
