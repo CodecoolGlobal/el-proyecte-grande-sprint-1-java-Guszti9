7e01cb813b506acd00c241aa5a896194d8bcf873
benedekmolnar <molnar.benedek@gmail.com>
2022-05-31 14:02:03 +0200

    Remove unused functions from App.js

--- react/src/App.js
+++ react/src/App.js
@@ -4,69 +4,6 @@ import React, {useEffect} from "react";
 import Footer from "./footer";
 import UserPage from "./userPage";
 
-
-function Ships() {
-    const [ships, setShips] = React.useState([]);
-
-    const getData = () => {
-        fetch("http://localhost:8080/api/spaceship/all", {
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                'Access-Control-Allow-Origin': 'http://localhost:8080'
-            }
-        }).then(function (response){
-            return response.json();
-        }).then(function (json){
-            console.log(json);
-            setShips(json);
-        });
-    }
-        useEffect(() => {
-            getData()
-        }, [])
-    return (
-            <div>
-                <ul>
-                    {ships.map((s) => <li>{s.name}</li>)}
-                </ul>
-            </div>
-        )
-}
-
-function Ship() {
-    const [ship, setShip] = React.useState([]);
-
-    const getData = () => {
-        fetch('http://localhost:8080/api/spaceship/0'
-            , {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json',
-                    'Access-Control-Allow-Origin': 'http://localhost:8080'
-                }
-            }
-        )
-            .then(function (response) {
-                // console.log(response)
-                return response.json();
-            })
-            .then(function (myJson) {
-                // console.log(myJson);
-                setShip(myJson)
-            });
-    }
-    useEffect(() => {
-        getData()
-    }, [])
-
-    return (
-        <div>
-            <p>{ship.name}</p>
-        </div>
-    )
-}
-
 function App() {
   return (
     <div className="App">
