97cebe4d38d5a890dbefbda1149270558a51131f
galambati <galambosati98@gmail.com>
2022-05-31 13:17:07 +0200

    initialize userPage, userDetails and footer components, add css.

--- react/src/App.js
+++ react/src/App.js
@@ -1,59 +1,94 @@
 import logo from './logo.svg';
 import './App.css';
 import React, {useEffect} from "react";
+import Footer from "./footer";
+import UserPage from "./userPage";
 
-function Ship(){
+
+function Ships() {
+    const [ships, setShips] = React.useState([]);
+
+    const getData = () => {
+        fetch("http://localhost:8080/api/spaceship/all", {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Access-Control-Allow-Origin': 'http://localhost:8080'
+            }
+        }).then(function (response){
+            return response.json();
+        }).then(function (json){
+            console.log(json);
+            setShips(json);
+        });
+    }
+        useEffect(() => {
+            getData()
+        }, [])
+    return (
+            <div>
+                <ul>
+                    {ships.map((s) => <li>{s.name}</li>)}
+                </ul>
+            </div>
+        )
+}
+
+function Ship() {
     const [ship, setShip] = React.useState([]);
 
-    const getData=()=>{
+    const getData = () => {
         fetch('http://localhost:8080/api/spaceship/0'
-            ,{
-                headers : {
+            , {
+                headers: {
                     'Content-Type': 'application/json',
                     'Accept': 'application/json',
-                    'Access-Control-Allow-Origin' : 'http://localhost:8080'
+                    'Access-Control-Allow-Origin': 'http://localhost:8080'
                 }
             }
         )
-            .then(function(response){
-                console.log(response)
+            .then(function (response) {
+                // console.log(response)
                 return response.json();
             })
-            .then(function(myJson) {
-                console.log(myJson);
+            .then(function (myJson) {
+                // console.log(myJson);
                 setShip(myJson)
             });
     }
-    useEffect(()=>{
+    useEffect(() => {
         getData()
-    },[])
+    }, [])
 
-  return (
-      <div>
-        <p>{ship.name}</p>
-      </div>
-  )
+    return (
+        <div>
+            <p>{ship.name}</p>
+        </div>
+    )
 }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Ship />
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    {/*<Ship/>*/}
+                    <Ships/>
+                </a>
+                    <UserPage/>
+                    <Footer/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ react/src/userDetails.js
@@ -0,0 +1,14 @@
+function userDetails(){
+    return (
+        <div id={"user-details"}>
+            <ul>
+                <li>detail1</li>
+                <li>detail2</li>
+                <li>detail3</li>
+                <li>detail4</li>
+            </ul>
+        </div>
+    )
+}
+
+export default userDetails;
