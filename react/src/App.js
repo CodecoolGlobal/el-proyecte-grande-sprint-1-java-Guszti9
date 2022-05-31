0d9390c724973921878d479f9d39ae144f4496f7
galambati <galambosati98@gmail.com>
2022-05-31 14:18:33 +0200

    implement footer look, add footer css

--- react/src/App.js
+++ react/src/App.js
@@ -70,23 +70,27 @@ function Ship() {
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    {/*<Ship/>*/}
-                    <Ships/>
-                </a>
-                    <UserPage/>
-                    <Footer/>
-            </header>
+            {/*<header className="App-header">*/}
+            {/*    <img src={logo} className="App-logo" alt="logo"/>*/}
+            {/*    <p>*/}
+            {/*        Edit <code>src/App.js</code> and save to reload.*/}
+            {/*    </p>*/}
+            {/*    <a*/}
+            {/*        className="App-link"*/}
+            {/*        href="https://reactjs.org"*/}
+            {/*        target="_blank"*/}
+            {/*        rel="noopener noreferrer"*/}
+            {/*    >*/}
+            {/*        /!*<Ship/>*!/*/}
+            {/*        <Ships/>*/}
+            {/*    </a>*/}
+            {/*        */}
+            {/*        <Footer/>*/}
+            {/*</header>*/}
+            <body>
+            <UserPage/>
+            <Footer/>
+            </body>
         </div>
     );
 }
--- react/src/userDetails.js
+++ react/src/userDetails.js
@@ -1,12 +1,35 @@
+import './userPage.css';
+
 function userDetails(){
     return (
         <div id={"user-details"}>
-            <ul>
-                <li>detail1</li>
-                <li>detail2</li>
-                <li>detail3</li>
-                <li>detail4</li>
-            </ul>
+            <table className="blueTable">
+                <thead>
+                <tr>
+                    <th>UserName</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>cell1_1</td>
+                </tr>
+                <tr>
+                    <td>cell1_2</td>
+                </tr>
+                <tr>
+                    <td>cell1_3</td>
+                </tr>
+                <tr>
+                    <td>cell1_4</td>
+                </tr>
+                <tr>
+                    <td>cell1_5</td>
+                </tr>
+                <tr>
+                    <td>cell1_6</td>
+                </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
