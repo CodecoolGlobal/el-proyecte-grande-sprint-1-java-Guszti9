fa6b7ad52540d42ef09ee7768baeb8f0fb82badb
Guszti9 <tusi0531@gmail.com>
2022-06-01 14:28:13 +0200

    add arrows to ship picture

--- react/src/vehicleDetails/shipImage.js
+++ react/src/vehicleDetails/shipImage.js
@@ -1,6 +1,19 @@
 function ShipImage(props) {
+
+
+
     return (
-        <img src={props.image} />
+        <div className="image-container">
+            <div className="left-arrow arrow">
+                <img src="shipDetailsImages/leftArrow.png"/>
+            </div>
+            <div className="ship-picture">
+                <img src={props.image} />
+            </div>
+            <div className="right-arrow arrow">
+                <img src="shipDetailsImages/rightArrow.png"/>
+            </div>
+        </div>
     );
 }
 
