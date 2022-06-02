a320c2276d85ba032fc854a8be5d3151f598c912
benedekmolnar <molnar.benedek@gmail.com>
2022-06-02 13:37:19 +0200

    Outsource selector functions from mainPage.js

--- react/src/main_page/mainPage.js
+++ react/src/main_page/mainPage.js
@@ -1,6 +1,7 @@
 import './mainPage.css';
 import {useEffect, useState} from "react";
 import {convertFunctions} from "./convertFunctions";
+import {YearSelect, WeaponSelect, MassSelect, PriceSelect, ClassSelect, FuelTypeSelect, ManufacturerSelect} from "./selectors";
 
 async function apiPost(url, payload) {
     let response = await fetch(url, {
@@ -29,133 +30,6 @@ async function filter(year, weapons, mass, price, classification, fuelType, manu
         fuelType: fuelType ? fuelType : null,
         manufacturer: manufacturer ? manufacturer : null};
     return await apiPost("http://localhost:8080/api/spaceship/filter", payLoad);
-}
-
-
-function YearSelect({filter}){
-    const [year, setYear] = useState(["Year", "< 2500", "2500-3000", "3000-3500", "3500-4000", "4000 <"])
-    const yearList = year.map(Year => Year)
-    const handleYearChange = (e) => filter(yearList[e.target.value]);
-
-    return (
-        <div className="select">
-        < select
-            onChange={e => handleYearChange(e)}
-            className="year" >
-            {
-                yearList.map((address, key) => <option key={key} value={key}>{address}</option>)
-            }
-        </select >
-        </div>
-    )
-}
-
-function WeaponSelect({filter}){
-    const [weapon, setWeapons] = useState(["Weapons", "WEAPONIZED", "NO WEAPONS"])
-    const weaponList = weapon.map(Weapon => Weapon);
-    const handleWeaponsChange = (e) => filter(weaponList[e.target.value]);
-
-    return (
-        <div className="select">
-            < select
-                onChange={e => handleWeaponsChange(e)}
-                className="weapon" >
-                {
-                    weaponList.map((address, key) => <option key={key} value={key}>{address}</option>)
-                }
-            </select >
-        </div>
-    )
-}
-
-function MassSelect({filter}){
-    const [mass, setMass] = useState(["Mass", "< 50.000", "50.000-100.000", "100.000 <"]);
-    const massList = mass.map(Mass => Mass);
-    const handleMassChange = (e) => filter(massList[e.target.value]);
-
-    return (
-        <div className="select">
-            < select
-                onChange={e => handleMassChange(e)}
-                className="mass" >
-                {
-                    massList.map((address, key) => <option key={key} value={key}>{address}</option>)
-                }
-            </select >
-        </div>
-    )
-}
-
-function PriceSelect({filter}){
-    const [price, setPrice] = useState(["Price", "< 100.000", "100.000-500.000", "500.000-1.000.000", "1.000.000 <"]);
-    const priceList = price.map(Price => Price);
-    const handlePriceChange = (e) => filter(priceList[e.target.value]);
-
-    return (
-        <div className="select">
-            < select
-                onChange={e => handlePriceChange(e)}
-                className="price" >
-                {
-                    priceList.map((address, key) => <option key={key} value={key}>{address}</option>)
-                }
-            </select >
-        </div>
-    )
-}
-
-function ClassSelect({filter}){
-    const [classification, setClassification] = useState(["Classification", "COMBAT", "TRANSPORT", "EXPLORATION", "INDUSTRIAL", "SUPPORT", "COMPETITION", "GROUND", "MULTI"])
-    const classificationList = classification.map(Classification => Classification);
-    const handleClassificationChange = (e) => filter(classificationList[e.target.value]);
-
-    return (
-        <div className="select">
-            < select
-                onChange={e => handleClassificationChange(e)}
-                className="classification" >
-                {
-                    classificationList.map((address, key) => <option key={key} value={key}>{address}</option>)
-                }
-            </select >
-        </div>
-    )
-}
-
-function FuelTypeSelect({filter}){
-    const [fuelType, setFuelType] = useState(["Fuel Type", "DIESEL", "GASOLINE", "ELECTRIC", "NUCLEAR"]);
-    const fuelTypeList = fuelType.map(FuelType => FuelType);
-    const handleFuelTypeChange = (e) => filter(fuelTypeList[e.target.value]);
-
-    return (
-        <div className="select">
-            < select
-                onChange={e => handleFuelTypeChange(e)}
-                className="fuel-type" >
-                {
-                    fuelTypeList.map((address, key) => <option key={key} value={key}>{address}</option>)
-                }
-            </select >
-        </div>
-    )
-}
-
-function ManufacturerSelect({filter}){
-    const [manufacturer, setManufacturer] = useState(["Manufacturer", "AEGIS DYNAMICS", "ANVIL AEROSPACE", "CRUSADER INDUSTRIES", "DRAKE INTERPLANETARY", "GREYCAT INDUSTRIAL", "TUMBRIL"])
-    const manufacturerList = manufacturer.map(Manufacturer => Manufacturer);
-    const handleManufacturerChange = (e) => filter(manufacturerList[e.target.value]);
-
-    return (
-        <div className="select">
-            < select
-                onChange={e => handleManufacturerChange(e)}
-                className="manufacturer" >
-                {
-                    manufacturerList.map((address, key) => <option key={key} value={key}>{address}</option>)
-                }
-            </select >
-        </div>
-    )
 }
 
 function SearchContainer() {
